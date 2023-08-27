f7a779e0406ded02d4ea9e20a921f6f4534c8adc
Aleksandr Shapovalov <giiks131@gmail.com>
2023-08-27 20:42:08 +0200

    Initial Commit

---
+++ avitoTestiOS_Shapovalov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avitoTestiOS_Shapovalov
+//
+//  Created by Aleksandr Shapovalov on 27/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
