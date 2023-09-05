6da8dbae6984977de044ad485d1adc2b4bc89e60
Aleksandr Shapovalov <giiks131@gmail.com>
2023-09-05 15:35:31 +0200

    Fixed reusable cell

--- avitoTestiOS_Shapovalov/Extensions/UIImageView+Extensions.swift
+++ avitoTestiOS_Shapovalov/Extensions/UIImageView+Extensions.swift
@@ -7,15 +7,30 @@
 
 import UIKit
 
+private var imageLoadingUrl: UInt8 = 0
+
 extension UIImageView {
+    var loadingURL: URL? {
+        get {
+            return objc_getAssociatedObject(self, &imageLoadingUrl) as? URL
+        }
+        set {
+            objc_setAssociatedObject(self, &imageLoadingUrl, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+
     func loadImage(from url: URL, placeholder: UIImage? = nil, completion: @escaping () -> Void) {
         self.image = placeholder
+        loadingURL = url
+
         DispatchQueue.global().async { [weak self] in
             if let data = try? Data(contentsOf: url),
                let image = UIImage(data: data) {
                 DispatchQueue.main.async {
-                    self?.image = image
-                    completion()
+                    if self?.loadingURL == url {
+                        self?.image = image
+                        completion()
+                    }
                 }
             }
         }
