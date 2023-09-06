869e5ccaa70e6e1ccdfbb5f5e2685d60f0702d8e
Aleksandr Shapovalov <giiks131@gmail.com>
2023-09-06 11:30:39 +0200

    Updated doc for controllers

--- avitoTestiOS_Shapovalov/Controllers/ProductDetailViewController.swift
+++ avitoTestiOS_Shapovalov/Controllers/ProductDetailViewController.swift
@@ -7,46 +7,63 @@
 
 import UIKit
 
+/// A UIViewController subclass for displaying the details of a product.
 class ProductDetailViewController: UIViewController {
-    
+
+    // MARK: - Properties
+
+    /// Coordinator for navigation.
     var coordinator: MainCoordinator?
+
+    /// Custom view for displaying the product details.
     private let productDetailView = ProductDetailView()
+
+    /// Custom view for loading and error states.
     private var loadingView = LoadingView()
+
+    /// ViewModel for handling business logic.
     private let viewModel: ProductDetailViewModel
-    
+
+    // MARK: - Initialization
+
+    /// Initializes a new ProductDetailViewController.
+    /// - Parameter viewModel: The ViewModel to use.
     init(viewModel: ProductDetailViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
-    
+
+    /// Required initializer, not implemented.
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
+    // MARK: - Lifecycle Methods
+
+    /// Sets up the view hierarchy and layout.
     override func loadView() {
         view = productDetailView
     }
-    
+
+    /// Configures the view after loading.
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Подробности о товаре"
+
         setupLoadingView()
         setupRetryButton()
-        
+
+        // Update UI when ViewModel triggers it
         viewModel.updateUIHandler = { [weak self] in
             self?.updateUI()
         }
+
         viewModel.fetchData()
     }
-    
-    private func setupRetryButton() {
-        loadingView.retryButton.addTarget(self, action: #selector(retryFetchingData), for: .touchUpInside)
-    }
-    
-    @objc private func retryFetchingData() {
-        viewModel.fetchData()
-    }
-    
+
+    // MARK: - Setup Methods
+
+    /// Sets up the loading view.
     private func setupLoadingView() {
         view.addSubview(loadingView)
         loadingView.translatesAutoresizingMaskIntoConstraints = false
@@ -57,9 +74,20 @@ class ProductDetailViewController: UIViewController {
             loadingView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
     }
-    
+
+    /// Sets up the retry button's action.
+    private func setupRetryButton() {
+        loadingView.retryButton.addTarget(self, action: #selector(retryFetchingData), for: .touchUpInside)
+    }
+
+    /// Fetches data again after a retry action.
+    @objc private func retryFetchingData() {
+        viewModel.fetchData()
+    }
+
     // MARK: - UI Update
-    
+
+    /// Updates the UI based on the ViewModel's state.
     private func updateUI() {
         switch viewModel.viewState {
         case .loading:
@@ -80,11 +108,15 @@ class ProductDetailViewController: UIViewController {
             }
         }
     }
-    
+
+    /// Sets the alpha value for the product detail view.
+    /// - Parameter alpha: The alpha value.
     private func setProductDetailViewAlpha(to alpha: CGFloat) {
         productDetailView.scrollView.alpha = alpha
     }
-    
+
+    /// Configures the UI elements with the given model.
+    /// - Parameter model: The model containing the data.
     private func configureUI(with model: AdvertisementDetailUIModel) async {
         await productDetailView.productImageView.loadImage(from: model.imageUrl, placeholder: UIImage(named: "placeholder"))
         self.productDetailView.titleLabel.text = model.title
--- avitoTestiOS_Shapovalov/Controllers/ProductListViewController.swift
+++ avitoTestiOS_Shapovalov/Controllers/ProductListViewController.swift
@@ -7,50 +7,66 @@
 
 import UIKit
 
+/// A UIViewController subclass for displaying the list of products.
 class ProductListViewController: UIViewController {
-    
+
+    // MARK: - Properties
+
+    /// Coordinator for navigation.
     var coordinator: MainCoordinator?
+
+    /// Custom view for displaying the product list.
     private let productListView = ProductListView()
+
+    /// Custom view for loading and error states.
     private var loadingView = LoadingView()
+
+    /// UIRefreshControl for pull-to-refresh functionality.
     private let refreshControl = UIRefreshControl()
+
+    /// ViewModel for handling business logic.
     private let viewModel: ProductListViewModel
-    
+
+    // MARK: - Initialization
+
+    /// Initializes a new ProductListViewController.
+    /// - Parameter viewModel: The ViewModel to use.
     init(viewModel: ProductListViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
-    
+
+    /// Required initializer, not implemented.
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
+    // MARK: - Lifecycle Methods
+
+    /// Sets up the view hierarchy and layout.
     override func loadView() {
         view = productListView
     }
-    
+
+    /// Configures the view after loading.
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Advertisements"
-        
+
         setupLoadingView()
         setupRetryButton()
         setupCollectionView()
-        
+
         viewModel.updateUIHandler = { [weak self] in
             self?.updateUI()
         }
-        
+
         viewModel.fetchData()
     }
-    
-    private func setupRetryButton() {
-        loadingView.retryButton.addTarget(self, action: #selector(retryFetchingData), for: .touchUpInside)
-    }
-    
-    @objc private func retryFetchingData() {
-        viewModel.fetchData()
-    }
-    
+
+    // MARK: - Setup Methods
+
+    /// Sets up the loading view.
     private func setupLoadingView() {
         view.addSubview(loadingView)
         loadingView.translatesAutoresizingMaskIntoConstraints = false
@@ -61,41 +77,54 @@ class ProductListViewController: UIViewController {
             loadingView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
     }
-    
+
+    /// Sets up the retry button's action.
+    private func setupRetryButton() {
+        loadingView.retryButton.addTarget(self, action: #selector(retryFetchingData), for: .touchUpInside)
+    }
+
+    /// Fetches data again after a retry action.
+    @objc private func retryFetchingData() {
+        viewModel.fetchData()
+    }
+
+    /// Sets up the collection view.
     private func setupCollectionView() {
         productListView.collectionView.delegate = self
         productListView.collectionView.dataSource = self
         productListView.collectionView.register(ProductCollectionViewCell.self, forCellWithReuseIdentifier: ProductCollectionViewCell.identifier)
-        
+
         let layout = createCompositionalLayout()
         productListView.collectionView.collectionViewLayout = layout
-        
+
         refreshControl.addTarget(self, action: #selector(reloadData), for: .valueChanged)
         productListView.collectionView.refreshControl = refreshControl
     }
-    
+
+    /// Creates a compositional layout for the collection view.
     private func createCompositionalLayout() -> UICollectionViewLayout {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1.0))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
+
         let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(350))
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        
+
         let section = NSCollectionLayoutSection(group: group)
         section.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
         section.interGroupSpacing = 10
-        
+
         let layout = UICollectionViewCompositionalLayout(section: section)
         return layout
     }
-    
+
+    /// Reloads the data when the refresh control is activated.
     @objc private func reloadData() {
         viewModel.fetchData { [weak self] in
             self?.refreshControl.endRefreshing()
         }
     }
-    
-    
+
+    /// Updates the UI based on the ViewModel's state.
     private func updateUI() {
         switch viewModel.viewState {
         case .loading:
@@ -113,12 +142,15 @@ class ProductListViewController: UIViewController {
 }
 
 // MARK: - UICollectionViewDelegate & Data Source
+
 extension ProductListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    
+
+    /// Returns the number of items to display in a given section.
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.advertisementUIModels.count
     }
-    
+
+    /// Returns a configured cell for a given index path.
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductCollectionViewCell.identifier, for: indexPath) as! ProductCollectionViewCell
         cell.prepareForReuse()
@@ -128,8 +160,8 @@ extension ProductListViewController: UICollectionViewDelegate, UICollectionViewD
         }
         return cell
     }
-    
-    
+
+    /// Handles cell selection to navigate to product details.
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let selectedAdvertisement = viewModel.advertisementUIModels[indexPath.row]
         coordinator?.navigateToProductDetail(with: selectedAdvertisement.id)
