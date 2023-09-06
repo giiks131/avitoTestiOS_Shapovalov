395f14ab89aab645fd75981c5966f2efc82da110
Aleksandr Shapovalov <giiks131@gmail.com>
2023-09-06 11:33:06 +0200

    Updated doc for viewmodels

--- avitoTestiOS_Shapovalov/ViewModels/ProductListViewModel.swift
+++ avitoTestiOS_Shapovalov/ViewModels/ProductListViewModel.swift
@@ -7,27 +7,46 @@
 
 import Foundation
 
+/// ViewModel responsible for managing the product list.
 @MainActor
 class ProductListViewModel {
+
+    // MARK: - Properties
+
+    /// Service for fetching advertisement data.
     private var advertisementService: AdvertisementService
+
+    /// Array of advertisement models fetched from the service.
     private(set) var advertisements: [AdvertisementModel] = []
+
+    /// Array of UI models derived from the advertisement models.
     private(set) var advertisementUIModels: [AdvertisementUIModel] = []
-    
+
+    /// Current state of the view.
     var viewState: ViewState = .loading {
         didSet {
             self.updateUIHandler?()
         }
     }
-    
+
+    /// Closure to trigger the UI update.
     var updateUIHandler: (() -> Void)?
-    
+
+    // MARK: - Initialization
+
+    /// Initializes the ViewModel with an AdvertisementService.
+    /// - Parameter advertisementService: The service for fetching advertisements.
     init(advertisementService: AdvertisementService) {
         self.advertisementService = advertisementService
     }
-    
+
+    // MARK: - Data Fetching
+
+    /// Fetches the advertisement data.
+    /// - Parameter completion: Optional completion handler.
     func fetchData(completion: @escaping () -> Void = {}) {
         viewState = .loading
-        
+
         // Try to get data from cache first
         if let cachedData: [AdvertisementModel] = CacheManager.shared.get(key: "AdvertisementsCache", type: [AdvertisementModel].self) {
             self.advertisements = cachedData
@@ -36,13 +55,13 @@ class ProductListViewModel {
             completion()
             return
         }
-        
+
         Task {
             do {
                 self.advertisements = try await advertisementService.fetchAdvertisements()
                 self.transformToUIModels()
                 self.viewState = .content
-                
+
                 // Save data to cache
                 CacheManager.shared.set(key: "AdvertisementsCache", value: self.advertisements)
             } catch let error as NetworkError {
@@ -53,7 +72,10 @@ class ProductListViewModel {
             completion()
         }
     }
-    
+
+    // MARK: - Helpers
+
+    /// Transforms advertisement models to UI models.
     private func transformToUIModels() {
         self.advertisementUIModels = self.advertisements.map { AdvertisementUIModel(from: $0) }
     }
