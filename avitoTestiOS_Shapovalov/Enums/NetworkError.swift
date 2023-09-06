294891ae14299599bfcf1f7c6c8215e01b215128
Aleksandr Shapovalov <giiks131@gmail.com>
2023-09-06 11:39:28 +0200

    Added doc to enums

--- avitoTestiOS_Shapovalov/Enums/NetworkError.swift
+++ avitoTestiOS_Shapovalov/Enums/NetworkError.swift
@@ -7,12 +7,16 @@
 
 import Foundation
 
-/// Custom errors to handle various network-related failures
+/// Custom enum to represent network-related errors.
+/// - badURL: Indicates that the URL is incorrect or malformed
+/// - noData: Indicates that no data was received from the server
+/// - decodingError: Indicates that an error occurred while decoding the received data
 enum NetworkError: Error, LocalizedError {
     case badURL
     case noData
     case decodingError
-    
+
+    /// Provides a localized description of the error.
     var errorDescription: String? {
         switch self {
         case .badURL:
